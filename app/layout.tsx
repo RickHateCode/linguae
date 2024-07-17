f60eee334182c39c7a26360f1e8e068a1a91b36a
rickHateCode <hanonyme99@gmail.com>
2024-07-17 19:57:55 +0100

    rickupdates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './ui/globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 // import {inter, merienda, nunito_sans} from "./lib/fonts";
 
 export default function RootLayout({
@@ -9,7 +10,7 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body className=''>{children} <SpeedInsights /></body>
+        <body className=''>{children} <SpeedInsights /> <Analytics /></body>
       </html>
     )
   }
